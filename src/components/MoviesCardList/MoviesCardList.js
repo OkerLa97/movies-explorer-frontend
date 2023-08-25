deabdd11a37e26e8d56154cd8c6778a4dfc47e8a
Oker <oker97@mail.ru>
2023-08-25 07:12:03 +0200

    Загрузка карточек в соответствии с разрешение экрана и рядов видео

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -1,19 +1,35 @@
 import React from "react";
 import Preloader from "../Preloader/Preloader";
 import MoviesCard from "../MoviesCard/MoviesCard";
-import { DESKTOP_MAX_CARDS_CNT, MOBILE_MAX_CARDS_CNT } from "../../utils/constants";
+import {
+  DESKTOP_RESOLUTION,
+  DESKTOP_CARDS_CNT,
+  DESKTOP_LOAD_MORE_CARDS_CNT,
+
+  TAB_CARDS_CNT,
+  TAB_LOAD_MORE_CARDS_CNT,
+
+  MOBILE_RESOLUTION,
+  MOBILE_CARDS_CNT,
+  MOBILE_LOAD_MORE_CARDS_CNT
+} from "../../utils/constants";
 
 class MoviesCardList extends React.Component {
 
   constructor(props){
+
     super(props);
     this.state = {
       showedFilms: [],
       currentIdx: 0,
     }
+
+    this.calculateLoadMoreCardsCnt();
+    window.addEventListener('resize', this.calculateLoadMoreCardsCnt);
   }
 
   maxCardsCnt = 0;
+  loadMoreCardsCnt = 0;
 
   componentDidUpdate(prevProps){
 
@@ -28,14 +44,32 @@ class MoviesCardList extends React.Component {
   // HANDLE функции
   handleMoreClick = () => {
     this.setState({
-      currentIdx: this.state.currentIdx + this.maxCardsCnt,
+      currentIdx: this.state.currentIdx + this.loadMoreCardsCnt,
     })
+  }
+
+  calculateLoadMoreCardsCnt = () => {
+    // ОПРЕДЕЛЯЕМ МАКСИМАЛЬНОЕ КОЛИЧЕСТВО КАРТОЧЕК В ЗАВИСИМОСТИ ОТ РАЗМЕРА ЭКРАНА
+    this.maxCardsCnt = DESKTOP_CARDS_CNT;
+    this.loadMoreCardsCnt = DESKTOP_LOAD_MORE_CARDS_CNT;
+    if(window.innerWidth <= DESKTOP_RESOLUTION && window.innerWidth > MOBILE_RESOLUTION){
+      this.maxCardsCnt = TAB_CARDS_CNT;
+      this.loadMoreCardsCnt = TAB_LOAD_MORE_CARDS_CNT;
+    }
+
+    else if(window.innerWidth <= MOBILE_RESOLUTION){
+      this.maxCardsCnt = MOBILE_CARDS_CNT;
+      this.loadMoreCardsCnt = MOBILE_LOAD_MORE_CARDS_CNT;
+    }
   }
 
   render(){
 
-    // ОПРЕДЕЛЯЕМ МАКСИМАЛЬНОЕ КОЛИЧЕСТВО КАРТОЧЕК В ЗАВИСИМОСТИ ОТ РАЗМЕРА ЭКРАНА
-    this.maxCardsCnt = window.innerWidth >= 768 ? DESKTOP_MAX_CARDS_CNT : MOBILE_MAX_CARDS_CNT;
+    // ОПРЕДЕЛЯЕМ КЛАСС ДЛЯ КОЛИЧЕСТВА КОЛОНОК
+
+
+    console.log(this.maxCardsCnt);
+    console.log(this.props.films);
 
     let dataPull = this.props.films;
 
@@ -48,11 +82,13 @@ class MoviesCardList extends React.Component {
     return (
       <section className="movie-card-list">
         {!this.props.moviesLoaded && this.props.searchQuery !== "" && <Preloader />}
-        {this.props.moviesLoadingError && <p className="movie-card-list__error">Во время запроса произошла ошибка. Возможно, проблема с соединением или сервер недоступен. Подождите немного и попробуйте ещё раз</p>}
-        {showedFilms.length === 0 && this.props.moviesLoaded && this.props.searchQuery !== "" && !this.props.moviesLoadingError && <p className="movie-card-list__error">Ничего не найдено</p>}
-        {showedFilms.map((film, index) => {
-          return <MoviesCard key={index} film={film} isSavedMovies={this.props.isSavedMovies} onLikeClick={this.props.onLikeClick}/>
-        })}
+        <div class='movie-card-list-container'>
+          {this.props.moviesLoadingError && <p className="movie-card-list__error">Во время запроса произошла ошибка. Возможно, проблема с соединением или сервер недоступен. Подождите немного и попробуйте ещё раз</p>}
+          {showedFilms.length === 0 && this.props.moviesLoaded && this.props.searchQuery !== "" && !this.props.moviesLoadingError && <p className="movie-card-list__error">Ничего не найдено</p>}
+          {showedFilms.map((film, index) => {
+            return <MoviesCard key={index} film={film} isSavedMovies={this.props.isSavedMovies} onLikeClick={this.props.onLikeClick}/>
+          })}
+        </div>
         {endIdx !== dataPull.length && <button className="movie-card-list__more-button" onClick={this.handleMoreClick}>Ещё</button>}
       </section>
     )
